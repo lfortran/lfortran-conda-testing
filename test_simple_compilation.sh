1decd62a76cbb08224422b617cb115b41ff24aa0
Ondřej Čertík <ondrej@certik.us>
2025-07-02 21:50:40 -0600

    X path

--- test_simple_compilation.sh
+++ test_simple_compilation.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-export PATH="$CONDA_PREFIX/Library/bin:$ORIGINAL_PATH"
+#export PATH="$CONDA_PREFIX/Library/bin:$ORIGINAL_PATH"
+export PATH="/c/Users/runneradmin/micromamba/envs/lf/Library/bin:$ORIGINAL_PATH"
 
 cd lfortran/examples
 lfortran expr2.f90
